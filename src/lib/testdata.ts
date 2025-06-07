f2a777d951d6bfc2e3d1d029985586146b5acaa3
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 02:55:43 +0900

    feat: 책 상세 정보 조회 백엔드 연결

--- src/lib/testdata.ts
+++ src/lib/testdata.ts
@@ -4,10 +4,6 @@ const books: Book[] = Array.from({ length: 47 }, (_, i) => (
   { id: `blah${i + 1}`, title: `The Pragmatic Programmer ${i + 1}`, author: "Andy Hunt", price: 5, quantity: 10, }
 ));
 
-export function getBookById(id: string) {
-  return books.find((book) => book.id === id);
-}
-
 export function updateBook(updated: Book) {
   const index = books.findIndex((book) => book.id === updated.id);
   if (index !== -1) {
--- src/routes/books/[id]/+page.server.ts
+++ src/routes/books/[id]/+page.server.ts
@@ -1,15 +1,15 @@
-import {getBookById} from "$lib/testdata";
 import type {Book} from "$lib/types/book";
 import {redirect} from "@sveltejs/kit";
 import {httpStatus} from "$lib/status";
 
 export const load = async ({params}) => {
   const id = params.id;
-  const book: Book | undefined = getBookById(id);
+  const response = await fetch(`http://localhost:8080/api/books/${id}`);
 
-  if (book === undefined) {
+  if (!response.ok) {
     throw redirect(httpStatus.TEMPORARY_REDIRECT, "/books");
   }
 
+  const book: Book = await response.json();
   return {book};
 };
