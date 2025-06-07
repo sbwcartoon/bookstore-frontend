1c102ae3dc950a97796d2d715a7955ad26572b84
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 03:17:39 +0900

    feat: 책 삭제 예외 처리

--- src/routes/books/[id]/+page.svelte
+++ src/routes/books/[id]/+page.svelte
@@ -52,9 +52,20 @@
   async function handleDelete(e: Event) {
     e.preventDefault();
     if (confirm("삭제하시겠습니까?")) {
-      await fetch(`/api/books/${book.id}`, {
+      const response = await fetch(`/api/books/${book.id}`, {
         method: "delete",
       });
+
+      if (!response.ok) {
+        if (response.status === httpStatus.NOT_FOUND) {
+          alert("책이 존재하지 않습니다. 저장된 책을 삭제해 주세요.");
+          throw await HttpError.fromResponse(response);
+        }
+
+        alert("오류가 발생했습니다. 잠시 후 다시 시도해 주세요.\n문제가 지속될 경우 관리자에게 문의해 주세요.");
+        throw await HttpError.fromResponse(response);
+      }
+
       alert("삭제되었습니다.");
       await goto("/books");
     }
