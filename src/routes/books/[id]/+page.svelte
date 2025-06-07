7a886046dab16bb4c78ec6a7ccb9835ae835af35
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 03:06:54 +0900

    feat: 책 수정 예외 처리

--- src/routes/books/[id]/+page.svelte
+++ src/routes/books/[id]/+page.svelte
@@ -3,6 +3,8 @@
   import type {Book} from "$lib/types/book";
   import {isNotBlank, isPositiveNaturalNumber} from "$lib/util/validation";
   import {ValidationError} from "$lib/exception/ValidationError";
+  import {httpStatus} from "$lib/status";
+  import {HttpError} from "$lib/exception/HttpError";
 
   const {data} = $props();
   const book: Book = $state(data.book);
@@ -19,10 +21,31 @@
       alert("입력 정보를 확인해주세요.");
       throw new ValidationError();
     }
-    await fetch(`/api/books/${book.id}`, {
+    const response = await fetch(`/api/books/${book.id}`, {
       method: "put",
       body: JSON.stringify(book),
     });
+
+    if (!response.ok) {
+      if (response.status === httpStatus.CONFLICT) {
+        alert("동일한 저자가 쓴 같은 제목의 책이 이미 존재합니다. 다른 내용으로 다시 시도해주세요.");
+        throw await HttpError.fromResponse(response);
+      }
+
+      if (response.status === httpStatus.BAD_REQUEST) {
+        alert("입력한 내용이 유효하지 않습니다. 올바르게 입력한 후 다시 시도해주세요.");
+        throw await HttpError.fromResponse(response);
+      }
+
+      if (response.status === httpStatus.NOT_FOUND) {
+        alert("책이 존재하지 않습니다. 저장된 책을 수정해 주세요.");
+        throw await HttpError.fromResponse(response);
+      }
+
+      alert("오류가 발생했습니다. 잠시 후 다시 시도해 주세요.\n문제가 지속될 경우 관리자에게 문의해 주세요.");
+      throw await HttpError.fromResponse(response);
+    }
+
     alert("수정되었습니다.");
   }
 
