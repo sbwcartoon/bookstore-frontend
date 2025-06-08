88ee8c3b45ac8ae946d80daf4acb221b7ce8b8f5
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 21:22:56 +0900

    feat: 타이틀 클릭 시 메인 페이지 이동

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,9 @@
 
 <div class="flex flex-col min-h-screen">
   <header class="bg-blue-600 text-white fixed top-0 w-full z-10 p-4">
-    <h1 class="text-xl font-bold">📚 RGT 온라인 서점</h1>
+    <h1 class="text-xl font-bold">
+      <a href="/">📚 RGT 온라인 서점</a>
+    </h1>
   </header>
 
   <main class="flex-1 overflow-y-auto pt-24 pb-12">
