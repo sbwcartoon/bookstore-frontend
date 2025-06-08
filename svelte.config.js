9488b354f36c9f8f7ba1e182df33d0862da745d6
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 11:31:20 +0900

    chore: vercel 배포를 위한 adapter 추가 및 배포 환경 분리

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = process.env.PUBLIC_ADAPTER === "vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: isVercel ? adapterVercel() : adapterNode(),
+  },
 };
 
 export default config;
