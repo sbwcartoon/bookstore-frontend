acba016c7aad17b2fb9ac75fea5f097b60dcbe33
sbwcartoon <sbwcartoon@gmail.com>
2025-06-08 16:48:16 +0900

    refactor: 검색 버튼 구조 개선

---
+++ src/lib/components/button/BlackSubmitButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const {children}: { children: () => any } = $props();
+</script>
+
+<button
+  type="submit"
+  class="bg-black text-white px-4 py-2 min-w-[100px] rounded whitespace-nowrap"
+>
+  {@render children()}
+</button>
--- src/lib/components/searchfilter/SearchFilter.svelte
+++ src/lib/components/searchfilter/SearchFilter.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import BlackSubmitButton from "$lib/components/button/BlackSubmitButton.svelte";
+
   let {
     title = $bindable<string>(),
     author = $bindable<string>(),
@@ -8,6 +10,5 @@
 <div class="flex justify-center items-center gap-1.5">
   <input type="text" bind:value={title} class="form-input w-full" placeholder="제목"/>
   <input type="text" bind:value={author} class="form-input w-full" placeholder="저자"/>
-  <button type="submit" class="px-10 py-2 whitespace-nowrap bg-black hover:bg-gray-900 text-white rounded">검색
-  </button>
+  <BlackSubmitButton>검색</BlackSubmitButton>
 </div>
